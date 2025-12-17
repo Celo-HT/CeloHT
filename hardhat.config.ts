afd06ccd224deb392eac0e2c9b1381ff9cf71bf4
CeloHT <celoht3@gmail.com>
2025-12-17 08:54:56 -0400

    Crear hardhat.config.ts

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    celo: {
+      url: process.env.CELO_RPC || "https://forno.celo.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.CELOSCAN_API_KEY || "",
+  },
+};
+
+export default config;
